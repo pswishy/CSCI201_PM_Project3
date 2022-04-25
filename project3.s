4f128cb5ae351cac054482b1f612358668266ff3
Pierce Medy <piercemedy24@gmail.com>
2022-04-25 18:35:06 -0400

    testing what happens with jal and jr

--- project3.s
+++ project3.s
@@ -20,7 +20,7 @@ while:
     lb $s1 0($t9)
 
     # abcd; abcd;   abcd;
-    beq $t1, 7, exit
+    beq $t1, 1000, exit
     beq $s1, 9, tabOrSpaceCharFound # if the char is a tab we have to give special consideration
     beq $s1, 32, tabOrSpaceCharFound # 32 = space char, 9 = tab char
       # if we get here we are at a char and no more leading whitespace
@@ -36,27 +36,37 @@ LeadingWhiteSpaceCounter:
 tabOrSpaceCharFound:
       # if it is a tab or space char and len $t0 is 0 then we want to ignore because it is a leading whitespace 
       beq $t0, 0 LeadingWhiteSpaceCounter
-      beq $t0, 0, skip # if t0 equals 0 leading whitespace dont update length of userinput string
+      # bgt $t0, 0,  # if t0 equals 0 leading whitespace dont update length of userinput string
       
 subStringFound:
 
-    # add $t2, $t2, 1 # increment number of substrings
     # sub $a0, $t9, $t2,
-    sub $a0, $a2, $t0 # find address of substring we need to substract length of string - number of substraings
-    sub $sp, $sp, $t0 # make room on stack
-    sw $ra 0($sp)
+    # sub $a0, $a2, $t0 # find address of substring we need to substract length of string - number of substraings
+    # sub $sp, $sp, $t0 # make room on stack
+    # sw $ra 0($sp)
     jal sub_a
+    # ***** dont forget to restore stack *****
     li $t0, 0 # set t0(length) back to 0
-    
+    # lw $ra, 0($sp)
+    # add $sp, $sp, $t0
+    j exit
     # add $t9, $t9, 1 # increment loop address
 # if number of substrings = 0 do what we did project 2
 
 sub_a:
-jal sub_b
-j exit
+    jal sub_b
+    j exit
+# t2 will hold length of string
+sub_b: # sub b needs to do calculations and return val
+    # bgt $t0, 4, codetesting
+    lb $s0 0($a0) # we need to know length of string to do calculations also need to account for trailing whitespace
+    # add $t2, $t2, 1 # everytime we get charcater add 1 to length of substring 
+    jr $ra
 
-sub_b:
-jr $ra
+# substringToLong:
+
+#    li $v0,0
+#     jr $ra
 
 skip:
       addi $t9, $t9, 1 # increment loop address for loop
@@ -75,3 +85,10 @@ print:
 exit:
       li $v0, 10
       syscall
+
+codetesting:
+    jr $ra
+      li $v0, 4
+      la $a0, testing
+      syscall
+       j exit

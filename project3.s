7b102a46a5b8f8b2443da4c895db486ed35d4b9e
Pierce Medy <piercemedy24@gmail.com>
2022-04-25 11:35:08 -0400

    initialize register values

--- project3.s
+++ project3.s
@@ -10,3 +10,11 @@ main:
       la $a0, user_input # stores user in put in $a0
       li $a1, 1000 # max lengeth of user input string
       syscall
+
+      # first step count how many chars are in userinput while ignoring blank and space tabs
+      li $t0, 0 # t0 = length of user input string
+      li $t1, 0 # iterator variable
+      li $t3, 33 # base for my program
+      li $t4, 0 # length counter
+      li $t5, 0 # leading white space counter
+      li $t8, 0 # sum variable
